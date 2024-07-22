d34c08a1ce0e1ff4bb107f25514ebdc1b6b1cb97
Bichav <veronicaheredia9@gmail.com>
2024-07-22 15:21:46 -0300

    cambios22_1

---
+++ BDE/modulo5/Material de Práctica 5/clase5.sql
@@ -0,0 +1,6 @@
+﻿-- CORRER ESTE ARCHIVO PARA REGISTRAR LAS COLUMNAS GEOMETRICAS DE LAS TRES NUEVAS TABLAS CON GEOMETRIAS
+
+INSERT INTO geometry_columns VALUES ('', 'salta', 'transporte_especial_nuevo', 'the_geom', 2, 22183, 'MULTILINESTRING');
+INSERT INTO geometry_columns VALUES ('', 'salta', 'carriles', 'the_geom', 2, 22183, 'MULTILINESTRING');
+
+
---
+++ BDE/modulo6/Material de Práctica 6/clase6.sql
@@ -0,0 +1,4 @@
+﻿select populate_geometry_columns ('trenes'::regclass);
+select populate_geometry_columns ('rutas'::regclass);
+select populate_geometry_columns ('salud2010.v_centros_datos'::regclass);
+select populate_geometry_columns ('salud2010.v_envolvente_ambulancia'::regclass);
